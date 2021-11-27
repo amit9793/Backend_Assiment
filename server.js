835087d6cfaca7b50e3a93e65927bfd19b64aad6
amit9793 <86410160+amit9793@users.noreply.github.com>
2021-11-27 11:10:45 +0530

    mongoose assiment

--- server.js
+++ server.js
@@ -1,56 +1,59 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
+const connect = () => {
+  return mongoose.connect("mongodb://127.0.0.1:27017/web11");
+};
+
+const newschema = new mongoose.Schema({
+  id: { type: Number, required: true },
+  movies_name: { type: String, required: true },
+  movies_genre: { type: String, required: true },
+  production_year: { type: Number, required: true },
+  budget: { type: Number, required: true },
+});
+
+const apple = mongoose.model("assiment", newschema);
 const app = express();
-
 app.use(express.json());
-const user = require("./MOCK_DATA.json");
 
-
-
-
-app.get("/", (req, res) => {
-  res.send(user);
+app.post("/user", async (req, res) => {
+  const data = await apple.create(req.body);
+  return res.status(201).send({ data });
 });
 
+// app.get("/user/:id", async (req, res) => {
+//   const data = await apple.find({ id : req.params.id }).lean().exec();
+//   if (data.length > 0) {
+//     return res.status(201).send(data);
+//   } else {
+//     return res.status(201).send("This Data Is Not Present in My Data Base");
+//   }
+// });
 
+// app.get("/user/:movies_name", async (req, res) => {
+//   const data = await apple.find({ movies_name:{$ne:"req.params.movies_name"}  }).lean().exec();
+//   if (data.length > 0) {
+//     return res.status(201).send(data);
+//   } else {
+//     return res.status(201).send("This Data Is Not Present in My Data Base");
+//   }
+// });
 
-app.post("/books", (req, res) => {
-  const newuser = [...user, req.body];
-  res.send(newuser);
-});
-
-
-
-
-app.get("/books/:author", (req, res) => {
-  const newuser = user.filter((user) => user.author === req.params.author);
-  res.send(newuser);
-});
-
-
-
-
-app.patch("/book/:id", (req, res) => {
-  const newuser = user.map((user) => {
-    if (+req.params.id === user.id) {
-      if (req?.body?.year) user.year = req.body.year;
-    }
-    return user;
-  });
-  res.send(newuser);
-});
-
-
-
-
-app.delete("/books/:id", (req, res) => {
-  const newuser = user.filter((user) => user.id !== +req.params.id);
-  res.send(newuser);
+app.get("/user/:production_year", async (req, res) => {
+  const data = await apple.find({$or:[{budget:{$gt:req.params.production_year}},{budget:req.params.production_year}]}).lean().exec();
+  if (data.length > 0) {
+    return res.status(201).send(data);
+  } else {
+    return res.status(201).send("This Data Is Not Present in My Data Base");
+  }
 });
 
 
 
 
-app.listen(2345, function () {
-  console.log("port are running");
+
+app.listen(2345, async function () {
+  await connect();
+  console.log("port is running");
 });
